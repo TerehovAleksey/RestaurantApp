98de5ebaf7b08a0c963b2d6ec04e080c3576afbb
Алексей Терехов <terehoff240885@outlook.com>
2019-09-20 00:33:44 +0300

    solution structure fix

---
+++ Domain/ApplicationUser.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Domain
+{
+    /// <summary>
+    /// Пользователь приложения
+    /// </summary>
+    public class ApplicationUser : IdentityUser
+    {
+
+    }
+}
